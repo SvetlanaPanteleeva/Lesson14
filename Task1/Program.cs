7bcd8e5765021e8c1c084ae24a378223a4c0d543
Светлана Пантелеева <panteleevas@gmail.com>
2023-02-09 01:14:53 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Animal cat = new Cat("Мурка", "Мяу");
+                cat.ShowInfo();
+                Console.WriteLine();
+                Animal dog = new Dog("Барбос", "Гав");
+                dog.ShowInfo();
+                Console.WriteLine();
+                Console.ReadKey();
+        }
+    }
+}
